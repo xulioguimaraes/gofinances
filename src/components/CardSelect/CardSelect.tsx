349411455dbad3fd886183bc4a114fb9dc12099c
Julio <despairnatural@gmail.com>
2022-08-13 16:33:15 -0400

    Correção 13

--- src/components/CardSelect/CardSelect.tsx
+++ src/components/CardSelect/CardSelect.tsx
@@ -4,7 +4,7 @@ import { Pressable } from "react-native";
 interface CardSelectProps {
   setImageSelect: React.Dispatch<React.SetStateAction<number>>;
   imageSelect: number;
-  textImage: string
+  textImage: string;
   value: number;
 }
 export const CardSelect = ({
@@ -43,18 +43,26 @@ export const CardSelect = ({
           />
         </Box>
       </Pressable>
-      <Box bg={{
-        linearGradient: {
-          colors:[ "gray.500", "transparent"],
-          start: [0, 1],
-      end: [0.1, 0],
-      location:[20, 80]
-        }
-      }} left={0} pl={2} pr={3} pt={2} bottom={0} position={"absolute"}>
-        <Heading color={"text.900"} textTransform={"capitalize"}>{textImage}</Heading>
+      <Box
+        bg={{
+          linearGradient: {
+            colors: ["gray.500", "transparent"],
+            start: [0, 1],
+            end: [0.1, 0],
+            location: [20, 80],
+          },
+        }}
+        left={0}
+        pl={2}
+        pr={3}
+        pt={2}
+        bottom={0}
+        position={"absolute"}
+      >
+        <Heading color={"text.900"} textTransform={"capitalize"}>
+          {textImage}
+        </Heading>
       </Box>
     </Center>
   );
 };
-// background: rgb(0,0,0);
-// background: linear-gradient(54deg, rgba(0,0,0,1) 0%, rgba(255,255,255,0) 100%);
